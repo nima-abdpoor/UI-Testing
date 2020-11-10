d0bb1af6952e036affc8c182375f6b19fe659a7f
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-11-10 18:34:17 +0330

    MainActivityTest.kt test completed.

--- app/src/androidTest/java/com/nima/uitesting/MainActivityTest.kt
+++ app/src/androidTest/java/com/nima/uitesting/MainActivityTest.kt
@@ -3,8 +3,7 @@ package com.nima.uitesting
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,7 +12,36 @@ import org.junit.runner.RunWith
 class MainActivityTest{
     @Test
     fun isActivityInIViewTest(){
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.main)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun titleVisibilityTest(){
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.activity_main_title)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+    @Test
+    fun buttonVisibilityTest(){
+        ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun titleTextTest(){
+        ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.activity_main_title)).check(matches(withText(R.string.text_mainactivity)))
+    }
+
+    @Test
+    fun buttonTextTest(){
+        ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity)).check(matches(withText(R.string.text_next)))
     }
 }
