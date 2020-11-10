595c85fd70349932126ecb937b630bb4c47dfde1
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-11-10 18:48:34 +0330

    secondActivity Test completed.

--- app/src/main/java/com/nima/uitesting/SecondActivity.kt
+++ app/src/main/java/com/nima/uitesting/SecondActivity.kt
@@ -8,6 +8,7 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
         button_back.setOnClickListener{
             this.onBackPressed()
         }
