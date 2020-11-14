94e342aae4d29f65f77621d130cb02b964e7921c
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-11-14 18:15:37 +0330

    run multiple test classes with TestSuit.

---
+++ app/src/androidTest/java/com/nima/uitesting/ActivitySuitTest.kt
@@ -0,0 +1,12 @@
+package com.nima.uitesting
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    MainActivityTest::class,
+    SecondActivityTest::class
+)
+class ActivitySuitTest {
+}
