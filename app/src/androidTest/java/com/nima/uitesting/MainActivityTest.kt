4915610772e7cb040ecceda7c3ba02a88b1856fb
nima-abdpoor <57753080+nima-abdpoor@users.noreply.github.com>
2020-11-14 18:09:46 +0330

    navigation test from main to secondary.

--- app/src/androidTest/java/com/nima/uitesting/MainActivityTest.kt
+++ app/src/androidTest/java/com/nima/uitesting/MainActivityTest.kt
@@ -2,6 +2,8 @@ package com.nima.uitesting
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -44,4 +46,26 @@ class MainActivityTest{
 
         onView(withId(R.id.button_next_activity)).check(matches(withText(R.string.text_next)))
     }
+    @Test
+    fun navSecondaryActivityTest(){
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun navBackActivityPressed(){
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.button_next_activity)).perform(click())
+
+        //method 1
+        //onView(withId(R.id.button_back)).perform(click())
+
+        //method 2
+        pressBack()
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+
+    }
+
 }
